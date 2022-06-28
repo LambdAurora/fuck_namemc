ee53699131faa2e2d3d6138b61c970397812ab12
LambdAurora <email@lambdaurora.dev>
2022-06-28 22:28:23 +0200

    Update buildscript and other stuff to modern standards.

--- src/main/java/dev/lambdaurora/fuck_namemc/FuckNameMC.java
+++ src/main/java/dev/lambdaurora/fuck_namemc/FuckNameMC.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 LambdAurora <aurora42lambda@gmail.com>
+ * Copyright (c) 2021 LambdAurora <email@lambdaurora.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
